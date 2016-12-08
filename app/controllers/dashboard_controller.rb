2f6c69f8cf5a785e042074bd365f6481f62704d8
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-08 14:13:10 -0700

    Pass users following to view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,9 @@ class DashboardController < ApplicationController
   def index
     @user = User.find(session[:user_id])
     @user_repo_data = GitHubUser.for_user(@user.token)
-    @user_followers_data = UserFollowers.followers(@user.username)
-    @user_stars = UserStars.stars(@user.username)
+    @user_followers_data = UserFollowers.followers(@user.token)
+    @user_stars = UserStars.stars(@user.token)
+    @user_following = UserFollowing.following(@user.token)
   end
 
 end
